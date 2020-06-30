42ed5c957d43a5e6351d7bd3f5becc8d5e945372
blake slochowsky <blakeslochowsky.com>
2020-06-30 13:57:38 -0500

    postman returning userprofile by ID

---
+++ Gifter/Controllers/PostController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Gifter.Data;
+using Gifter.Repositories;
+
+namespace Gifter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly PostRepository _postRepository;
+        public PostController(ApplicationDbContext context)
+        {
+            _postRepository = new PostRepository(context);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_postRepository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var post = _postRepository.GetById(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+
+        [HttpGet("getbyuser/{id}")]
+        public IActionResult GetByUser(int id)
+        {
+            return Ok(_postRepository.GetByUserProfileId(id));
+        }
+    }
+}
---
+++ Gifter/Repositories/PostRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Gifter.Data;
+using Gifter.Models;
+
+namespace Gifter.Repositories
+{
+    public class PostRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Post> GetAll()
+        {
+            return _context.Post.Include(p => p.UserProfile).ToList();
+        }
+
+        public Post GetById(int id)
+        {
+            return _context.Post.Include(p => p.UserProfile).FirstOrDefault(p => p.Id == id);
+        }
+        public List<Post> GetByUserProfileId(int id)
+        {
+            return _context.Post.Include(p => p.UserProfile)
+                            .Where(p => p.UserProfileId == id)
+                            .OrderBy(p => p.Title)
+                            .ToList();
+        }
+    }
+}
